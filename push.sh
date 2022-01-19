8c8a6dd6d65b5dc87e82a1d54b6a1ae026f450e7
莫名 <wb-hyx02133761@alibaba-inc.com>
2022-01-19 20:19:07 +0800

    update

--- push.sh
+++ push.sh
@@ -11,15 +11,12 @@ if [ ! -f ./themes/next/_config.yml ]; then
 	exit
 fi
 
-#将git秘钥替换，防止直接提交到git仓库上
-sed -i '' 's/<client_id>/635b7cd2c27103bfabd9/g' ./themes/next/_config.yml
-sed -i '' 's/<client_secret>/46e585c2bae1206fa16207c2caddfc53028cbc64/g' ./themes/next/_config.yml
+sed -i '' 's/635b7cd2c27103bfabd9/<client_id>/g' ./themes/next/_config.yml
+sed -i '' 's/46e585c2bae1206fa16207c2caddfc53028cbc64/<client_secret>/g' ./themes/next/_config.yml
 
 git add .
 git commit -m $1
 git push origin gh-pages
 
-sed -i '' 's/635b7cd2c27103bfabd9/<client_id>/g' ./themes/next/_config.yml
-sed -i '' 's/46e585c2bae1206fa16207c2caddfc53028cbc64/<client_secret>/g' ./themes/next/_config.yml
-
-
+sed -i '' 's/<client_id>/635b7cd2c27103bfabd9/g' ./themes/next/_config.yml
+sed -i '' 's/<client_secret>/46e585c2bae1206fa16207c2caddfc53028cbc64/g' ./themes/next/_config.yml
